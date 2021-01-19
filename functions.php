4a297fd809be6310490477183a29894730c88a46
Asyl Qwerty <pa@crocos.kz>
2021-01-19 13:48:15 +0600

    fix

---
+++ functions.php
@@ -0,0 +1,50 @@
+<?php
+
+function get_user_by_email(string $email) {
+    $pdo = new PDO("mysql:host=localhost;dbname=login", "root", "");
+    $sql = "SELECT * FROM registration WHERE email=:email";
+    $statement = $pdo->prepare($sql);
+    $statement -> execute(["email" => $email]);
+    $user = $statement->fetchAll(PDO::FETCH_ASSOC);
+    return $user;
+};
+
+function add_user(string $email, string $password) {
+    $pdo = new PDO("mysql:host=localhost;dbname=login", "root", "");
+    $sql = "INSERT INTO registration (email, password) VALUES (:email, :password)";
+    $statement = $pdo->prepare($sql);
+    $statement->execute([
+        "email" => $email,
+        "password" => password_hash($password, PASSWORD_DEFAULT)
+    ]);
+};
+
+function set_flash_message(string $key, string $message) {
+    $_SESSION[$key] = "$message";
+};
+
+function display_flash_message(string $key) {
+    if(isset($_SESSION[$key])):
+        echo "<div class=\"alert alert-{$key} text-dark\" role=\"alert\">";
+                echo $_SESSION[$key];
+                unset($_SESSION[$key]);
+       echo "</div>";
+    endif;
+};
+
+function redirect_to(string $path) {
+    header("Location:$path");
+    exit;
+};
+
+function login(string $email, string $password): bool {
+    $pdo = new PDO("mysql:host=localhost;dbname=login", "root", "");
+    $sql = "SELECT * FROM registration WHERE email=:email";
+    $statement = $pdo->prepare($sql);
+    $statement->execute(["email" => $email,]);
+    $user = $statement->fetchAll(PDO::FETCH_ASSOC);
+    // var_dump($user); die;
+    $user_password_hash = $user[0]['password'];
+    $password_verify = password_verify($password, $user_password_hash);
+    return $password_verify;
+};
---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require "functions.php";
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$login_verify = login($email, $password);
+
+if(!empty($login_verify)) {
+  set_flash_message("danger", "<strong>Уведомление!</strong> Неверный пароль или имя пользователя");
+  redirect_to("page_login.php");
+} else {
+  $_SESSION["email"] = $email;
+  set_flash_message("success", "Здравствуйте {$_SESSION["email"]}! Вы успешно авторизировались!");
+  redirect_to("page_login.php");
+}
+
