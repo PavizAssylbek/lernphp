dc72eb2c19c8203710fc3d7c044b645cfb646656
Asyl Qwerty <pa@crocos.kz>
2021-01-19 14:50:08 +0600

    fix login

--- login.php
+++ login.php
@@ -7,12 +7,11 @@ $password = $_POST['password'];
 
 $login_verify = login($email, $password);
 
-if(!empty($login_verify)) {
+if(empty($login_verify)) {
   set_flash_message("danger", "<strong>Уведомление!</strong> Неверный пароль или имя пользователя");
   redirect_to("page_login.php");
-} else {
-  $_SESSION["email"] = $email;
-  set_flash_message("success", "Здравствуйте {$_SESSION["email"]}! Вы успешно авторизировались!");
-  redirect_to("page_login.php");
 }
+$_SESSION["email"] = $email;
+set_flash_message("success", "Здравствуйте {$_SESSION["email"]}! Вы успешно авторизировались!");
+redirect_to("page_login.php");
 
